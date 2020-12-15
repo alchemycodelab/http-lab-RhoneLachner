8fbc4764625c0a2caa4dc8128f9135642e5a57a7
Rhone Lachner <fernandclay@gmail.com>
2020-12-14 19:47:35 -0800

    not sure why test are failing. Do I need a separate folder with additional routing and SQL?

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it.only('responds with hi', async() => {
+    const response = await request(app)
+      .get('/');
+    expect(response.text).toEqual('hi');
+  });
 
+
+  
 });
