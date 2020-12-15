31d830bc8de1cba35e79f71efcd710a577e6ff77
Rhone Lachner <fernandclay@gmail.com>
2020-12-14 19:52:46 -0800

    fixed a typo but app.js test still failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-  it.only('responds with hi', async() => {
+  it('responds with hi', async() => {
     const response = await request(app)
       .get('/');
     expect(response.text).toEqual('hi');
   });
 
-
+  it('posts status ')
   
 });
